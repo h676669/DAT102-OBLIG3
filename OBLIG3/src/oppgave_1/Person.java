b93bf0ede1f5b9f5479f0fe18c4f3349cf5ae1ab
h676669 <676669@stud.hvl.no>
2024-02-29 19:13:21 +0100

    skrev om testene på TabellMengde slik at de samsvarer med JavaSetToMengde

--- OBLIG3/src/oppgave_1/Person.java
+++ OBLIG3/src/oppgave_1/Person.java
@@ -1,2 +1,15 @@
-package oppgave_1;public class Person {
+package oppgave_1;
+
+public class Person {
+    private String navn;
+    private String [] Hobbyer;
+    public Person(String navn, String... hobbyer){
+        //skal sikkert vere noe her
+    }
+    static double match(Person A, Person B){
+        long match = 0;long antallFelles = 0;long antallKunHosDenEne = 0;long antallKunHosDenAndre = 0;long antallTotal = 0;
+
+        // all kode er bare start ting og skal sikkert bli slettet seinare med noe som faktisk gir mening
+        return (match = antallFelles - (antallKunHosDenEne + antallKunHosDenAndre) / antallTotal);
+    }
 }
