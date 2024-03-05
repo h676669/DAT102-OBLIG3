a5312c67f34325becfed3a20554d2b169f9efe5c
h676669 <676669@stud.hvl.no>
2024-03-05 08:40:41 +0100

    hei

--- Tests/TabellMengdeTest.java
+++ Tests/TabellMengdeTest.java
@@ -2,11 +2,7 @@ import oppgave_1.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 
 public class TabellMengdeTest {
 
