6dca8b218da7582da5faa34aca28b9416174349f
h676669 <676669@stud.hvl.no>
2024-03-07 22:25:36 +0100

    start paa oppgave 4(egentlig 2)

---
+++ OBLIG3/src/oppgave_4/BinaryVSHash.java
@@ -0,0 +1,15 @@
+package oppgave_4;
+
+import java.util.HashSet;
+
+public class BinaryVSHash {
+    private HashSet<Integer> Hash;
+    private Integer[] liste;
+
+    public BinaryVSHash(){
+        Hash = new HashSet<>(10);
+        liste = new Integer[10];
+    }
+
+
+}
