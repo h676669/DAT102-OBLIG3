9d2c46512888493f29ad4f740482b244089a169f
h676669 <676669@stud.hvl.no>
2024-03-08 12:47:27 +0100

    Start ting

--- OBLIG3/src/oppgave_4/BinaryVSHash.java
+++ OBLIG3/src/oppgave_4/BinaryVSHash.java
@@ -1,15 +1,25 @@
 package oppgave_4;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class BinaryVSHash {
     private HashSet<Integer> Hash;
-    private Integer[] liste;
+    private List<Integer> liste;
+    private static final int START_STORRELSE = 10;
 
     public BinaryVSHash(){
-        Hash = new HashSet<>(10);
-        liste = new Integer[10];
+        Hash = new HashSet<Integer>(START_STORRELSE);
+        liste = new ArrayList<Integer>(START_STORRELSE);
     }
 
+    public void SettInn(int antElement){
+        int tall = 376; // Her kan vi bruke eit vilkårlig tal
+        for (int i = 0; i < antElement;i++){
+            // legg tall til i HashSet og tabell
+            tall = (tall + 45713) % 1000000; // Sjå nedenfor om 45713
+        }
 
+    }
 }
